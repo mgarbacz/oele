7a645878112d75daffdc2f055521be87d333a602
mgarbacz <michal@garbacz.io>
2024-02-06 15:34:01 -0500

    Remove basic original textarea

--- app.js
+++
@@ -1,6 +0,0 @@
-const initialString = `
-  userType == 'EMPLOYEE' AND location == 'US' OR location != 'US'
-`;
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById('oele-input').textContent = initialString.trim();
-});
